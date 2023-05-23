0f26350dc305348e4b001e62159cd8246e6ff246
huyphan1108 <81922720+huyphan1108@users.noreply.github.com>
2023-05-23 09:42:38 -0400

    Add project files.

---
+++ SqlQuery_1.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE animals (
+    id integer PRIMARY KEY,
+    MMRC_id text NOT NULL,
+    species text NOT NULL,
+    name text NOT NULL,
+    DoB text NOT NULL,
+    age integer NOT NUll,
+    rescue_location text NOT NULL,
+    rescue_country text NOT NULL
+    );
+
+INSERT INTO animals VALUES (1,'EL-F-01', 'sea otter', 'Katmai', '08/17/2012', 10, 'Seward Alaska', 'USA');
+
+INSERT INTO animals VALUES (2,  'EL-M-01', 'nothern fur seal', 'MAk', '02-04-2016', 7, 'Kachemack Alaska', 'Canada');
+
+SELECT * FROM animals;
